3b638401e1a72686a0a373775ac3ba4d8454962d
Li Shi <shili2048@gmail.com>
2022-04-12 15:43:13 -0400

    Update tests

--- tests/tests.c
+++ tests/tests.c
@@ -17,9 +17,9 @@ int test2(int a, int b, int c, int d, int i) {
   // should be parallelizable into a vector add
   // <e, f, g, h> = <a, b, c, d> + bitcast(A[i:i+4])
   int e = a + A[i];
-  int f = b + A[i+1];
-  int g = c + A[i+2];
-  int h = d + A[i+3];
+  int f = b + A[i + 1];
+  int g = c + A[i + 2];
+  int h = d + A[i + 3];
 
   // now need to unpack
   return e + f + g + h;
@@ -29,32 +29,32 @@ int test3(int a, int b, int c, int d, int i) {
   // should be parallelizable into a vector add
   // <e, f, g, h> = <a, b, c, d> + <A[i], A[i+2], A[i+1], A[i+10]>
   int e = a + A[i];
-  int f = b + A[i+2];
-  int g = c + A[i+1];
-  int h = d + A[i+10];
+  int f = b + A[i + 2];
+  int g = c + A[i + 1];
+  int h = d + A[i + 10];
 
   // now need to unpack
   return e + f + g + h;
 }
 
-void test4() {
+void test4(int a, int b, int c, int d, int i) {
   // should be parallelizable into a vector add
   // bitcast(A[i:i+4]) = <a, b, c, d> + <a, b, c, d>
   A[i] = a + a;
-  A[i+1] = b + b;
-  A[i+2] = c + c;
-  A[i+3] = d + d;
+  A[i + 1] = b + b;
+  A[i + 2] = c + c;
+  A[i + 3] = d + d;
 
   return;
 }
 
-void test5() {
+void test5(int a, int b, int c, int d, int i) {
   // should be parallelizable into a vector add
   // bitcast(A[i:i+4]) = <a, b, c, d> + bitcast(A[i:i+4])
   A[i] = a + A[i];
-  A[i+1] = b + A[i+1];
-  A[i+2] = c + A[i+2];
-  A[i+3] = d + A[i+3];
+  A[i + 1] = b + A[i + 1];
+  A[i + 2] = c + A[i + 2];
+  A[i + 3] = d + A[i + 3];
 
   return;
 }
@@ -63,8 +63,8 @@ int main() {
   test1(1, 2, 3, 4);
   test2(1, 2, 3, 4, 0);
   test3(1, 2, 3, 4, 0);
-  test4();
-  test5();
-  
+  test4(1, 2, 3, 4, 0);
+  test5(1, 2, 3, 4, 0);
+
   return 0;
 }
