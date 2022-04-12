620b1e2f2072b48503465c27714d49eaab86ca95
Li Shi <shili2048@gmail.com>
2022-04-12 16:48:01 -0400

    Update tests and check SLP analysis pass without codeGen

--- tests/tests.c
+++ tests/tests.c
@@ -1,43 +1,43 @@
 #define N 16
-static long A[N];
+int A[N];
 
 int test1(int a, int b, int c, int d) {
   // should be parallelizable into a vector add
   // <e, f, g, h> = <a, b, c, d> + <a, b, c, d>
-  int e = a + a;
-  int f = b + b;
-  int g = c + c;
-  int h = d + d;
+  int e = a * a;
+  int f = b * b;
+  int g = c * c;
+  int h = d * d;
 
   // now need to unpack
   return e + f + g + h;
 }
 
-int test2(int a, int b, int c, int d, int i) {
+int test2(int a, int b, int c, int d, long i) {
   // should be parallelizable into a vector add
   // <e, f, g, h> = <a, b, c, d> + bitcast(A[i:i+4])
-  int e = a + A[i];
-  int f = b + A[i + 1];
-  int g = c + A[i + 2];
-  int h = d + A[i + 3];
+  int e = a * A[i];
+  int f = b * A[i + 1];
+  int g = c * A[i + 2];
+  int h = d * A[i + 3];
 
   // now need to unpack
   return e + f + g + h;
 }
 
-int test3(int a, int b, int c, int d, int i) {
+int test3(int a, int b, int c, int d, long i) {
   // should be parallelizable into a vector add
-  // <e, f, g, h> = <a, b, c, d> + <A[i], A[i+2], A[i+1], A[i+10]>
-  int e = a + A[i];
-  int f = b + A[i + 2];
-  int g = c + A[i + 1];
-  int h = d + A[i + 10];
+  // <e, f, g, h> = <a, b, c, d> + <A[i], A[i+1], A[i+2], A[i+3]>
+  int e = a * A[i];
+  int f = b * A[i + 1];
+  int g = c * A[i + 2];
+  int h = d * A[i + 3];
 
   // now need to unpack
   return e + f + g + h;
 }
 
-void test4(int a, int b, int c, int d, int i) {
+void test4(int a, int b, int c, int d, long i) {
   // should be parallelizable into a vector add
   // bitcast(A[i:i+4]) = <a, b, c, d> + <a, b, c, d>
   A[i] = a + a;
@@ -48,13 +48,13 @@ void test4(int a, int b, int c, int d, int i) {
   return;
 }
 
-void test5(int a, int b, int c, int d, int i) {
+void test5(int a, int b, int c, int d, long i) {
   // should be parallelizable into a vector add
   // bitcast(A[i:i+4]) = <a, b, c, d> + bitcast(A[i:i+4])
-  A[i] = a + A[i];
-  A[i + 1] = b + A[i + 1];
-  A[i + 2] = c + A[i + 2];
-  A[i + 3] = d + A[i + 3];
+  A[i] = a * A[i];
+  A[i + 1] = b * A[i + 1];
+  A[i + 2] = c * A[i + 2];
+  A[i + 3] = d * A[i + 3];
 
   return;
 }
